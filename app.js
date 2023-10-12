f41d1aef9260722764eb17076f28f6ed0baa262a
Matias-Diaz <diazmatias57@gmail.com>
2023-10-11 21:58:09 -0300

    cambios

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+let numeros = [1, 3, 3, 4, 5, 6, 7, 8];
 
+for (let i = 0; i < numeros.length; i++) {
+  //console.log(numeros[i] * 2);
+
+  if (numeros[i] % 2 == 0) {
+     console.log("es par");
+  } else {
+     console.log("es impar");
+  }
+}
